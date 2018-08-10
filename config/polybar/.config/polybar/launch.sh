c56800fdcd66a35ccfafef14ff49c80bb87e4f8c
Patrick Gomes Fernandes <patrick.gomes1@gmail.com>
2018-08-10 20:47:06 +0200

    Fix polybar launch. See https://github.com/jaagr/polybar/issues/763

--- config/polybar/.config/polybar/launch.sh
+++ config/polybar/.config/polybar/launch.sh
@@ -6,9 +6,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar example
-polybar -r top1 > /dev/null 2> ~/.config/polybar/top1-error.log &
-polybar -r top2 > /dev/null 2> ~/.config/polybar/top2-error.log &
+# Launch bar
 
-echo "Polybar launched"
-
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload topbar > /dev/null 2> ~/.config/polybar/topbar-$m.log & 
+done
